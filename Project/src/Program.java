51c8524e568065836d12a747b0923ccc2cbad07c
Javier Elizaga <javi-rider@hotmail.com>
2013-04-21 12:18:53 +0200

    minor changes

--- Project/src/Program.java
+++ Project/src/Program.java
@@ -5,7 +5,6 @@ import owl.OntologyClassificator;
 import patientCase.PatientCaseParser;
 
 public class Program {
-
 	private String path = "owlFiles/";
 	private String fileName = "icd10no.owl";
 	// private String fileName = "atc.owl";
@@ -25,8 +24,8 @@ public class Program {
 			System.out.println("Found " + hits.size() + " hits.");
 			for (int i = 0; i < hits.size(); ++i) {
 				System.out.println(hits.get(i).get("code"));
-				// System.out.println((i + 1) + ". " + hits.get(i).get("code")+
-				// "\t" + hits.get(i).get("text"));
+				// System.out.println((i + 1) + ". " +
+				// hits.get(i).get("code")+"\t" + hits.get(i).get("text"));
 			}
 		}
 
