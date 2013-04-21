730df79894e852e5eea147beb4d5a67917c581bc
javier-elizaga <javi-rider@hotmail.com>
2013-04-21 17:02:18 +0300

    Update PatientCaseParser.java

--- Project/src/patientCase/PatientCaseParser.java
+++ Project/src/patientCase/PatientCaseParser.java
@@ -7,16 +7,16 @@ import java.util.ArrayList;
 public class PatientCaseParser {
 
 	public PatientCaseParser() {
-		
+
 	}
-	
+
 	public PatientCase getCase(String caseName) {
 		PatientCase patientCase;
 		String text = readCase(caseName);
 		patientCase = new PatientCase(caseName, getTextLines(text));
 		return patientCase;
 	}
-	
+
 	private ArrayList<String> getTextLines(String fullText) {
 		String[] lines = fullText.split("\\. ");
 		ArrayList<String> temp = new ArrayList<String>();
@@ -24,34 +24,46 @@ public class PatientCaseParser {
 		for (int i = 1; i < lines.length; i++) {
 			if (lines[i].length() > 0) {
 				int firstLetter = lines[i].charAt(0);
-				//check if the next letter start with uppercase
-				if ((firstLetter <= 90 && firstLetter >= 65) || firstLetter == 197 || firstLetter == 198 || firstLetter == 216) {
+				// check if the next letter start with uppercase
+				if ((firstLetter <= 90 && firstLetter >= 65)
+						|| firstLetter == 197 || firstLetter == 198
+						|| firstLetter == 216) {
 					temp.add(lines[i]);
 				} else {
-					temp.set(temp.size() - 1, temp.get(temp.size() - 1) + ". " + lines[i]);
+					temp.set(temp.size() - 1, temp.get(temp.size() - 1) + ". "
+							+ lines[i]);
 				}
 			}
 		}
 		return temp;
 	}
 
-	
-	private static String readCase(String caseName){
+	private static String readCase(String caseName) {
 		String caseText = "";
 		String line = "";
 		try {
-			BufferedReader bReader = new BufferedReader(new FileReader("Cases/" + caseName + ".txt"));
+			BufferedReader bReader = new BufferedReader(new FileReader("Cases/"
+					+ caseName + ".txt"));
 			do {
+				if (!line.isEmpty()) {
+					while (line.charAt(line.length() - 1) == ' '
+							| line.charAt(line.length() - 1) == '\t') {
+						line = line.substring(0, line.length() - 1);
+					}
+					if (line.charAt(line.length() - 1) != '.') {
+						line += ".";
+					}
+					line += " ";
+				}
 				caseText += line;
 				line = bReader.readLine();
 			} while (line != null);
 			bReader.close();
-			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return caseText;	
+
+		return caseText;
 	}
 }
