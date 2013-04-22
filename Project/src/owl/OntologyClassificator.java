f28786e3ba73f875d1d1e4ff0512c1d4d6a99f54
javier-elizaga <javi-rider@hotmail.com>
2013-04-22 14:39:39 +0300

    Update OntologyClassificator.java

--- Project/src/owl/OntologyClassificator.java
+++ Project/src/owl/OntologyClassificator.java
@@ -1,9 +1,12 @@
 package owl;
 
+import handbook.Chapter;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 import org.apache.lucene.analysis.no.NorwegianAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -23,10 +26,6 @@ import org.apache.lucene.util.Version;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 public class OntologyClassificator {
-	private String path = "owlFiles/";
-	private String fileName = "icd10no.owl";
-	// private String fileName = "atc.owl";
-	private NorwegianAnalyzer analyzer;
 	private Directory index;
 
 	private static void addDoc(IndexWriter w, String text, String code)
@@ -36,12 +35,23 @@ public class OntologyClassificator {
 		doc.add(new StringField("code", code, Field.Store.YES));
 		w.addDocument(doc);
 	}
+	
+	private static void addDoc(IndexWriter w, ArrayList<String> icdCodes,
+			String path, String name) throws IOException {
+		Document doc = new Document();
+		String icdCodeString = "";
+		for (String code : icdCodes) {
+			icdCodeString += code + " ";
+		}
+		doc.add(new TextField("icdCodes", icdCodeString, Field.Store.NO));
+		doc.add(new StringField("path", path, Field.Store.YES));
+		doc.add(new StringField("name", name, Field.Store.YES));
+		w.addDocument(doc);
+	}
 
 	public OntologyClassificator(String path, String fileName) {
-		this.path = path;
-		this.fileName = fileName;
 		try {
-			index();
+			index(path, fileName);
 		} catch (OWLOntologyCreationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -51,25 +61,13 @@ public class OntologyClassificator {
 		}
 	}
 
-	public OntologyClassificator() {
-		try {
-			index();
-		} catch (OWLOntologyCreationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	private void index() throws OWLOntologyCreationException, IOException {
+	private void index(String path, String fileName)
+			throws OWLOntologyCreationException, IOException {
 		OwlParser owlParser = new OwlParser();
 		owlParser.parse(path, fileName);
 		ArrayList<OWL_Class> owl_classes = owlParser.getOwl_Classes();
-		// 1. index
 		index = new RAMDirectory();
-		analyzer = new NorwegianAnalyzer(Version.LUCENE_42);
+		NorwegianAnalyzer analyzer = new NorwegianAnalyzer(Version.LUCENE_42);
 		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_42,
 				analyzer);
 		IndexWriter w = new IndexWriter(index, config);
@@ -79,14 +77,12 @@ public class OntologyClassificator {
 		w.close();
 	}
 
-	public ArrayList<Document> searchLine(String querystr) {
+	public ArrayList<Document> search(String querystr) {
 		ArrayList<Document> returnDocs = new ArrayList<Document>();
 		try {
-			// the "text" argument specifies the default field to use
-			// when no field is explicitly specified in the query.
+			NorwegianAnalyzer analyzer = new NorwegianAnalyzer(Version.LUCENE_42);
 			Query q = new QueryParser(Version.LUCENE_42, "text", analyzer)
 					.parse(querystr);
-			// 3. search
 			int hitsPerPage = 10;
 			IndexReader reader = DirectoryReader.open(index);
 			IndexSearcher searcher = new IndexSearcher(reader);
@@ -97,20 +93,55 @@ public class OntologyClassificator {
 
 			for (int i = 0; i < hits.length; ++i) {
 				int docId = hits[i].doc;
-//				if (hits[i].score > 0.5) {
-					Document d = searcher.doc(docId);
-					returnDocs.add(d);
-//				} else
-//					break;
-				if (hits[i + 1].score < hits[i].score) {
+				Document d = searcher.doc(docId);
+				returnDocs.add(d);
+				if (i + 1 < hits.length && hits[i + 1].score < hits[i].score) {
 					break;
 				}
+			}
+			reader.close();
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return returnDocs;
+	}
+
+	public static ArrayList<Document> search(String querystr, Directory index) {
+		ArrayList<Document> returnDocs = new ArrayList<Document>();
+		try {
+			NorwegianAnalyzer analyzer = new NorwegianAnalyzer(Version.LUCENE_42);
+			Query q = new QueryParser(Version.LUCENE_42, "icdCodes", analyzer)
+					.parse(querystr);
+			int hitsPerPage = 10;
+			IndexReader reader = DirectoryReader.open(index);
+			IndexSearcher searcher = new IndexSearcher(reader);
+			TopScoreDocCollector collector = TopScoreDocCollector.create(
+					hitsPerPage, true);
+			searcher.search(q, collector);
+			ScoreDoc[] hits = collector.topDocs().scoreDocs;
+			for (int i = 0; i < hits.length - 1; ++i) {
+				int docId = hits[i].doc;
+				Document d = searcher.doc(docId);
+				returnDocs.add(d);
 			}
 
 			reader.close();
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.err.println(e.getMessage());
 		}
 		return returnDocs;
+	}
+
+	public static Directory createIndex(ArrayList<Chapter> allChapters) throws IOException {
+		Directory index = new RAMDirectory();
+		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_42);
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_42,
+				analyzer);
+		IndexWriter w = new IndexWriter(index, config);
+		for (Chapter c : allChapters) {
+			addDoc(w, c.getIcdCodes(), c.getPath(), c.getName());
+		}
+		w.close();
+		return index;
 	}
 }
